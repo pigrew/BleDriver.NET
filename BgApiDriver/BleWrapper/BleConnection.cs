f05a848af1703548b0a2954b97a45e2d1c46598b
NConrad <pigrew@gmail.com>
2016-11-12 16:13:22 -0500

    BleConnection: Remember the BLE address.

--- BgApiDriver/BleWrapper/BleConnection.cs
+++ BgApiDriver/BleWrapper/BleConnection.cs
@@ -7,9 +7,12 @@ namespace BgApiDriver.BleWrapper {
     public class BleConnection {
         public int ConnectionHandle { get; set; }
         public Dictionary<BleUUID, BleService> Services;
-        public BleConnection(int connectionHandle) {
+        bd_addr _Address;
+        public bd_addr Address { get { return _Address; } }
+        public BleConnection(int connectionHandle, bd_addr address) {
             this.ConnectionHandle = connectionHandle;
             Services = new Dictionary<BleUUID, BleService>();
+            this._Address = address;
         }
         public BleService FindServiceByHandle(int handle) {
             foreach(var s in Services) {
